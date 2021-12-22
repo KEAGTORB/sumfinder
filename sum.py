734f6965dd9530f3d2153d46fb8a909dd7604740
Minhaj <92469140+KEAGTORB@users.noreply.github.com>
2021-12-22 13:52:13 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,40 @@
+# colors
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
+# code
+print (cyan+b+"""
+
+
+
+
+
+
+░██████╗██╗░░░██╗███╗░░░███╗
+██╔════╝██║░░░██║████╗░████║
+╚█████╗░██║░░░██║██╔████╔██║
+░╚═══██╗██║░░░██║██║╚██╔╝██║
+██████╔╝╚██████╔╝██║░╚═╝░██║
+╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝
+
+
+
+
+
+
+
+"""+b+cyan)
+
+print (yellow+b+"              <===[[ coded by K34G2R8 ]]===>"+b+yellow)
+print (" ")
+print (gren+b+"        <---( Github-  KEAGTORB)--->"+b+gren)
+print (" ")
+
+
+First_number = int(input("Enter a Number : "))
+Second_Number = int(input("Enter a Second Number: "))
+Sum = First_number+Second_Number
+print("The Answer of " + str(First_number) + " + "  + str(Second_Number) + " is  : " + str(Sum))
